fbab87badf5eabcd4a059761a6c4e590f8ace60c
SP SFDC Remote <subham.1993.17@gmail.com>
2021-06-03 07:53:49 +0530

    adding events code

--- force-app/main/default/lwc/changeCase/changeCase.js
+++ force-app/main/default/lwc/changeCase/changeCase.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
-
-export default class ChangeCase extends LightningElement {}
+import { LightningElement, api, track } from 'lwc';
+ 
+export default class ChangeCase extends LightningElement {
+    @track updatedCase;
+ 
+    @api 
+    changeUpperCase(phrase){
+        this.updatedCase = phrase.toUpperCase();
+    }
+}
